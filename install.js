d4ef660f16819e8c39180df93e4e7b2a90c15484
Anand Gadiyar <gadiyar@ti.com>
2014-05-30 03:57:32 -0400

    Version 0.4.1.1
    Signed-off-by: Anand Gadiyar <gadiyar@ti.com>

--- install.js
+++ install.js
@@ -6,7 +6,7 @@ var XpiInstaller = {
 	// --- Editable items begin ---
 	extFullName: 'PDF Download', // The name displayed to the user (don't include the version)
 	extShortName: 'pdfdownload', // The leafname of the JAR file (without the .jar part)
-	extVersion: '0.4',
+	extVersion: '0.4.1.1',
 	extAuthor: 'Denis Remondini',
 	extLocaleNames: null, // e.g. ['en-US', 'en-GB']
 	extSkinNames: null, // e.g. ['classic', 'modern']
